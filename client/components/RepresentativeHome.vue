8798aa370be15205ba332bf45731a1b72580f67b
bayramBabagulyyew <babakulyyewbayram8@gmail.com>
2025-07-29 12:01:49 +0500

    :bug: fix: logo dealership

--- client/components/RepresentativeHome.vue
+++ client/components/RepresentativeHome.vue
@@ -48,7 +48,8 @@ export default {
     img {
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      padding: 5%;
+      object-fit: contain;
     }
     @media (max-width: 479px) {
       width: 60px;
