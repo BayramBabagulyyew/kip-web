21bd8a90003d33b82abf77d4897943331303d67b
bayramBabagulyyew <babakulyyewbayram8@gmail.com>
2025-09-15 15:32:11 +0500

    :bug: fix: slug util

--- server/src/partners/partner.module.ts
+++ server/src/partners/partner.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SlugUtil } from '@utils/slug.util';
 import { PartnerController } from './partner.controller';
 import { PartnerService } from './partner.service';
 
 @Module({
   controllers: [PartnerController],
-  providers: [PartnerService],
+  providers: [PartnerService, SlugUtil],
   exports: [PartnerService],
 })
 export class PartnerModule { }
